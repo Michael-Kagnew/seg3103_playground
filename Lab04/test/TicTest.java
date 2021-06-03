4648f716916d873a9b4bdc17099479bcf2252cdc
Michael Kagnew <mike.kagnew@gmail.com>
2021-06-03 18:37:52 -0400

    buggy code

--- Lab04/test/TicTest.java
+++ Lab04/test/TicTest.java
@@ -2,38 +2,47 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-import src.Tic;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.console.shadow.picocli.CommandLine.Parameters;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import src.Tic;
 
 @RunWith(Parameterized.class)
 public class TicTest{
+    
 
-    Tic obj = new Tic();
+    //The object
+    Tic obj;
 
-    public TicTest(int cols, int rows){
+    //The expected outcome
+    String[][] expected;
 
-
+    public TicTest(Object[][] ticVals, Object[][] expected){
+       
+        this.obj = new Tic((int)ticVals[0][0], (int)ticVals[0][1]);
+        this.expected = (String[][]) expected;
     }
 
+
     @Parameters
-    public static List<Tic[]> data(){
-        return null;
+    public static List<Object[][][]> data(){
+        List<Object[][][]> params = new LinkedList<Object[][][]>();
+        params.add(new Object[][][]{ {{2,3}}, {{"_|","_|", "_"}, {"_|","_"}}} );
+        params.add(new Object[][][]{ {{1,2}}, {{"_|","_"}}} );
+        params.add(new Object[][][]{ {{1,2}}, {{"_|","_"}, {"_|","_"}}} );
+        return params;
     }
 
 
 
     @Test
     public void boardTest(){
-
-        String[][] result = {{"_|","_|", "_"}, {"_|","_|", "_"}};
-
-        assertEquals(result, obj.emptyBoard(2,3));
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(obj.board));
     }
 }
