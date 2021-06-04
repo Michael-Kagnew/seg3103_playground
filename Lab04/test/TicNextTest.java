6d75cdc283764e56c2dd45b989686374f58b59b0
GabeCordo <gabe.cordovado@yahoo.ca>
2021-06-03 20:04:07 -0400

    buggy code

--- Lab04/test/TicNextTest.java
+++ Lab04/test/TicNextTest.java
@@ -15,18 +15,21 @@ import org.junit.runners.Parameterized.Parameters;
 import src.Tic;
 
 @RunWith(Parameterized.class)
-public class TicTest{
+public class TicNextTest {
     
 
     //The object
     Tic obj;
+    int nextTicX, nextTicY;
 
     //The expected outcome
     Object[][] expected;
 
-    public TicTest(Object[][] ticVals, Object[][] expected){
-       
+    public TicNextTest(Object[][] ticVals, Object[][] expected){
+        
         this.obj = new Tic((Integer)(ticVals[0][0]), (Integer)(ticVals[0][1]));
+        nextTicX = (Integer)ticVals[0][2];
+        nextTicY = (Integer)ticVals[0][3];
         
         this.expected = expected;
     }
@@ -35,24 +38,17 @@ public class TicTest{
     @Parameters
     public static List<Object[][][]> data(){
         List<Object[][][]> params = new LinkedList<Object[][][]>();
-        params.add(new Object[][][]{ {{2,3}}, {{"_|","_|", "_"}, {"_|","_|","_"}}} );
-        params.add(new Object[][][]{ {{1,2}}, {{"_|","_"}}} );
-        params.add(new Object[][][]{ {{2,2}}, {{"_|","_"}, {"_|","_"}}} );
-        params.add(new Object[][][]{ {{2,2}}, {{"_|","_"}, {"_|","_"}}} );
-        params.add(new Object[][][]{ {{3,3}}, {{"_|","_|", "_"}, {"_|","_|","_"}, {"_|","_|", "_"}}} );
+        params.add(new Object[][][]{ {{2,3,0,0}}, {{"X|","_|", "_"}, {"_|","_|","_"}}} );
+        params.add(new Object[][][]{ {{1,2,1,0}}, {{"_|","X"}}} );
+        params.add(new Object[][][]{ {{2,2,1,1}}, {{"_|","_"}, {"_|","X"}}} );
+        params.add(new Object[][][]{ {{2,2,1,0}}, {{"_|","X"}, {"_|","_"}}} );
+        params.add(new Object[][][]{ {{3,3,1,2}}, {{"_|","_|", "_"}, {"_|","_|","_"}, {"_|","X|", "_"}}} );
 
         return params;
     }
 
-
-
-    @Test
-    public void boardTest(){
-        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(obj.board));
-    }
-
     @Test
     public void tickBoardTest() {
-        assertEquals(expected, actual);
+        assertEquals(expected, obj.tickBoard(nextTicX, nextTicY, Tic.Player));
     }
 }
