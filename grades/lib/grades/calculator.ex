4f265d366bfe80fd358adbc950b3b6d8d3449e9e
Michael K <doubleshadowx1@gmail.com>
2021-06-06 17:16:41 -0400

    2.4 Refactor 1
    refactored the mark output for numeric grade and letter grade to make the code to be cleaner for those who look at those two functions

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -36,8 +36,46 @@ defmodule Grades.Calculator do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
   
- 
+  # Question 2.4 (Michael Kagnew)
+  # The formula for determining what the output of the function for letter_grade and numeric_grade makes the code within them much longer.
+  # Encapsulated both into one function that can be called, makes the code cleaner to look at.
+  #
+  #@param mark, marktype : int or string
+  #@return a string that represents the letter grade or an integer representing the numeric grade.
+  
+  def mark_output(mark, markType) do
+     if markType == "letter" do
+        cond do
+        mark > 0.895 -> "A+"
+        mark > 0.845 -> "A"
+        mark > 0.795 -> "A-"
+        mark > 0.745 -> "B+"
+        mark > 0.695 -> "B"
+        mark > 0.645 -> "C+"
+        mark > 0.595 -> "C"
+        mark > 0.545 -> "D+"
+        mark > 0.495 -> "D"
+        mark > 0.395 -> "E"
+        :else -> "F"
+        end
+      else
+        cond do 
+        mark > 0.895 -> 10
+        mark > 0.845 -> 9
+        mark > 0.795 -> 8
+        mark > 0.745 -> 7
+        mark > 0.695 -> 6
+        mark > 0.645 -> 5
+        mark > 0.595 -> 4
+        mark > 0.545 -> 3
+        mark > 0.495 -> 2
+        mark > 0.395 -> 1
+        :else -> 0
+        end
+    end
+  end
 
+ 
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)  # refractor 2.1
 
@@ -64,19 +102,20 @@ defmodule Grades.Calculator do
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)  # refractor 2.3
 
-      cond do
-        mark > 0.895 -> "A+"
-        mark > 0.845 -> "A"
-        mark > 0.795 -> "A-"
-        mark > 0.745 -> "B+"
-        mark > 0.695 -> "B"
-        mark > 0.645 -> "C+"
-        mark > 0.595 -> "C"
-        mark > 0.545 -> "D+"
-        mark > 0.495 -> "D"
-        mark > 0.395 -> "E"
-        :else -> "F"
-      end
+      # cond do
+      #   mark > 0.895 -> "A+"
+      #   mark > 0.845 -> "A"
+      #   mark > 0.795 -> "A-"
+      #   mark > 0.745 -> "B+"
+      #   mark > 0.695 -> "B"
+      #   mark > 0.645 -> "C+"
+      #   mark > 0.595 -> "C"
+      #   mark > 0.545 -> "D+"
+      #   mark > 0.495 -> "D"
+      #   mark > 0.395 -> "E"
+      #   :else -> "F"
+      # end
+      mark_output(mark, "letter")
     end
   end
 
@@ -97,19 +136,21 @@ defmodule Grades.Calculator do
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)  # refractor 2.3
 
-      cond do
-        mark > 0.895 -> 10
-        mark > 0.845 -> 9
-        mark > 0.795 -> 8
-        mark > 0.745 -> 7
-        mark > 0.695 -> 6
-        mark > 0.645 -> 5
-        mark > 0.595 -> 4
-        mark > 0.545 -> 3
-        mark > 0.495 -> 2
-        mark > 0.395 -> 1
-        :else -> 0
-      end
+      # cond do
+      #   mark > 0.895 -> 10
+      #   mark > 0.845 -> 9
+      #   mark > 0.795 -> 8
+      #   mark > 0.745 -> 7
+      #   mark > 0.695 -> 6
+      #   mark > 0.645 -> 5
+      #   mark > 0.595 -> 4
+      #   mark > 0.545 -> 3
+      #   mark > 0.495 -> 2
+      #   mark > 0.395 -> 1
+      #   :else -> 0
+      # end
+      mark_output(mark, "number")
+
     end
   end
 
