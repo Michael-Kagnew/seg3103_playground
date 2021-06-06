2d1bd4f8b481d440db698266b94a86df5d8a9df3
Michael K <doubleshadowx1@gmail.com>
2021-06-06 16:34:01 -0400

    Refactor 2.2
    wrapped the calculation if the student was able to particpate in marking in a single function to return true or false. Affects lines 60, and 93

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -26,6 +26,15 @@ defmodule Grades.Calculator do
   def calculate_grade(avg_labs, avg_homework, midterm, final) do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
+  
+    # Question 2.2 (Michael Kagnew)
+  # The formula used to determine if the student is able to participate in the marking scheme.
+  # @param avg_homework, avg_exams, num_labs : boolean
+  # @returns a boolean determining if the student is able to participate in the grade calculation.
+  #
+  def failed_to_participate(avg_homework, avg_exams, num_labs) do 
+     avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
+  end
 
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)  # refractor 2.1
@@ -48,7 +57,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate(avg_homework, avg_exams, num_labs) do #refractor 2.2
       "EIN"
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)  # refractor 2.3
@@ -81,7 +90,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate(avg_homework, avg_exams, num_labs) do #refractor 2.2
       0
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)  # refractor 2.3
@@ -101,4 +110,7 @@ defmodule Grades.Calculator do
       end
     end
   end
+
+
+
 end
